a9658f4d25b0fc7f525101c2cc7c82a255164d01
disha-agarwal <agarwal.disha21@gmail.com>
2021-05-05 02:15:11 +0000

    attack time update

--- restart.py
+++ restart.py
@@ -173,7 +173,7 @@ class Algorithm:
 
 
 ips = ["10.0.0.228","10.0.0.6", "10.0.0.204", "10.0.0.135", "10.0.0.198", "10.0.0.231"]
-attackTime = 90
+attackTime = 120
 rebootTime = 30
 t = 4
 stateFileName = "/home/ubuntu/reboot_code/reboot_state"
